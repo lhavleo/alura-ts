3a03b8cb6b5a4fe5ad6c155c7304d77bace310a7
Leonardo Valin <leonardo@serrabits.com.br>
2024-04-29 08:50:07 -0300

    aplicando typescript no código

---
+++ dist/js/main.ts
@@ -0,0 +1,50 @@
+import "./components/nova-transacao-component.js";
+import "./components/saldo-component.js";
+import "./components/extrato-component.js";
+import { TipoTransacao } from "./types/TipoTransacao.js";
+
+let saldo = 3000;
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+
+
+const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
+elementoFormulario.addEventListener("submit", function() {
+    /*event.preventDefault();
+    if(!elementoFormulario.checkValidity()) {
+        alert("Preencha todos os campos da transação!");
+        return;
+    }*/
+
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao") as HTMLSelectElement;
+    const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
+    const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
+
+    let tipoTransacao: string = inputTipoTransacao.value;
+    let valor: number = inputValor.valueAsNumber;
+    let data: Date = new Date(inputData.value);
+
+    if (tipoTransacao == "Depósito") {
+        saldo += valor;
+    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    } else {
+        alert("Tipo de Transação é inválida");
+        return;
+    }
+
+    elementoSaldo.textContent = saldo.toString();
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
